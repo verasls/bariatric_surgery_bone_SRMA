c450b196d63c2f7e75b89033f8c4da70e569ea68
Lucas <lucasdsveras@gmail.com>
2021-05-25 15:24:32 +0100

    Delete sd_from_se()

--- code/funs.R
+++ code/funs.R
@@ -11,19 +11,17 @@ compute_mean <- function(median, q1, q3) {
 
 # Compute standard deviation
 #
-# Compute the standard deviation from either the standard error, the
-# confidence interval or the interquartile range.
+# Compute the standard deviation from either the confidence interval or the
+# interquartile range.
 #
 # Args:
 #   from: A character string indicating from which statistic to compute the
-# standard deviation from. Can be either "se", "ci" or "iqr".
+# standard deviation from. Can be either "ci" or "iqr".
 #   n: The sample size.
 #   x_lower, x_upper: The lower and upper levels of the summary statistic
 # used to compute the standard deviation.
 compute_sd <- function(from, n, mean, x_lower = NA, x_upper = NA) {
-  if (from == "se") {
-    sd_from_se(n, mean, x_lower, x_upper)
-  } else if (from == "ci") {
+  if (from == "ci") {
     sd_from_ci(n, mean, x_lower, x_upper)
   } else if (from == "iqr") {
     sd_from_iqr(n, x_lower, x_upper)
@@ -45,13 +43,6 @@ compute_sd_mean_change <- function(sd_pre, sd_post, corr) {
 
 
 # Other aux functions below
-sd_from_se <- function(n, mean, se_lower, se_upper) {
-  sem1 <- mean - se_lower
-  sem2 <- se_upper - mean
-  sem <- (sem1 + sem2) / 2
-  sem * sqrt(n)
-}
-
 sd_from_ci <- function(n, mean, ci_lower, ci_upper) {
   sqrt(n) * (ci_upper - ci_lower) / (2 * qt(0.975, df = n - 1))
 }
