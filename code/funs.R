68611338a996a2a79a85f960d710664ee6bf6187
Lucas <lucasdsveras@gmail.com>
2021-05-27 23:23:22 +0100

    Add figure functions

--- code/funs.R
+++ code/funs.R
@@ -1,3 +1,5 @@
+# Data tranformation functions --------------------------------------------
+
 # Compute mean
 #
 # Compute the mean value based on the median, 1st and 3rd quartiles.
@@ -49,4 +51,65 @@ sd_from_ci <- function(n, mean, ci_lower, ci_upper) {
 
 sd_from_iqr <- function(n, q1, q3) {
   (q3 - q1) / (2 * qnorm((0.75 * n - 0.125) / (n + 0.25)))
+}
+
+# Figure functions --------------------------------------------------------
+
+# Build the text that forms the follow-up column in the forest plot
+#
+# Args:
+#   data: A data.frame or escalc object.
+#   x: The x coordinate to plot the text.
+followup_text <- function(data, x) {
+  k <- nrow(data) + 1
+  for (i in seq(nrow(data), 1, -1)) {
+    text(
+      x, i, font = 1, pos = 2, cex = 1,
+      data$time_after_surgery[k - i]
+    )
+  }
+}
+
+# Build the text that forms the sample size column in the forest plot
+#
+# Args:
+#   data: A data.frame or escalc object.
+#   x: The x coordinate to plot the text.
+n_text <- function(data, x) {
+  k <- nrow(data) + 1
+  for (i in seq(nrow(data), 1, -1)) {
+    text(
+      x, i, font = 1, pos = 2, cex = 1,
+      data$n[k - i]
+    )
+  }
+}
+
+# Build the text that forms the weights column in the forest plot
+#
+# Args:
+#   model: An rma object.
+#   x: The x coordinate to plot the text.
+weights_text <- function(model, x) {
+  w <- unname(weights(model))
+  k <- length(w) + 1
+  for (i in seq(length(w), 1, -1)) {
+    text(
+      x, i, font = 1, pos = 2, cex = 1,
+      paste0(broman::myround(w[k - i], 2), "%")
+    )
+  }
+}
+
+# Compute the I^2
+#
+# Args:
+#   data: A data.frame or escalc object.
+#   model: An rma object.
+i2 <- function(data, model) {
+  W <- diag(1 / data$vi)
+  X <- model.matrix(model)
+  P <- W - W %*% X %*% solve(t(X) %*% W %*% X) %*% t(X) %*% W
+  100 * sum(model$sigma2) /
+    (sum(model$sigma2) + (model$k - model$p) / sum(diag(P)))
 }
