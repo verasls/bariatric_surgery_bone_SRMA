d638e6f40d0c51f1e92e97536bcb2e6474db2a98
Lucas <lucasdsveras@gmail.com>
2021-06-04 12:04:46 +0100

    Reorder plots

--- figures/forest_plots.R
+++ figures/forest_plots.R
@@ -19,6 +19,126 @@ load(here("output", "ma_objects.rda"))
 # transformation was done to achieve a better control in the plot grid using
 # {cowplot}.
 
+# Total hip vBMD forest plot -------------------------------------------
+
+make_TH_plot <- function() {
+  par(mar = c(3.5, 2, 1, 2))
+  forest(
+    TH_vBMD_model,
+    slab = TH_vBMD$study,
+    header = TRUE,
+    digits = 1,
+    xlab = "",
+    mlab = "Random effects model",
+    xlim = c(-70, 59),
+    top = 4
+  )
+  # X axis label
+  text(11, -5, font = 1, pos = 2, cex = 1, "Mean percentage change")
+  # Plot title
+  text(
+    -70.5, 6, font = 2, pos = 4, cex = 1,
+    "Total hip volumetric bone mineral density"
+  )
+  # Follow-up time column
+  text(-26, 5, font = 2, pos = 2, cex = 1, "Follow-up (mo.)")
+  followup_text(TH_vBMD, -27)
+  # Sample size column
+  text(-22, 5, font = 2, pos = 2, cex = 1, "n")
+  n_text(TH_vBMD, -22)
+  # Weight column
+  text(34, 5, font = 2, pos = 2, cex = 1, "Weight")
+  weights_text(TH_vBMD_model, 34)
+  text(34, -1, font = 1, pos = 2, cex = 1, "100.0%")
+  # Heterogeneity text
+  text(
+    -69.9, -2.1, pos = 4, cex = 0.85,
+    bquote(
+      paste(
+        "Heterogeneity: ",
+        I^2, " = ",
+        .(
+          formatC(i2(TH_vBMD, TH_vBMD_model), digits = 0, format = "f")
+          ), "%, ",
+        italic(.("p")), " = ",
+        .(formatC(TH_vBMD_model$QEp, digits = 3, format = "f"))
+      )
+    )
+  )
+  # Overall effect text
+  text(
+    -69.9, -3.0, pos = 4, cex = 0.85,
+    bquote(
+      paste(
+        "Test for overall effect: ",
+        "z = ",
+        .(formatC(TH_vBMD_model$zval, digits = 2, format = "f")), ", ",
+        italic(.("p")), " = ",
+        .(formatC(TH_vBMD_model$pval, digits = 3, format = "f"))
+      )
+    )
+  )
+}
+
+# Lumbar spine vBMD forest plot -------------------------------------------
+
+make_LS_plot <- function() {
+  par(mar = c(2.5, 2, 0.5, 2))
+  forest(
+    LS_vBMD_model,
+    slab = LS_vBMD$study,
+    header = TRUE,
+    digits = 1,
+    xlab = "",
+    mlab = "Random effects model",
+    xlim = c(-70, 59),
+    top = 4
+  )
+  # X axis label
+  text(15, -5.5, font = 1, pos = 2, cex = 1, "Mean percentage change")
+  # Plot title
+  text(
+    -70.5, 13.2, font = 2, pos = 4, cex = 1,
+    "Lumbar spine volumetric bone mineral density"
+  )
+  # Follow-up time column
+  text(-26, 12, font = 2, pos = 2, cex = 1, "Follow-up (mo.)")
+  followup_text(LS_vBMD, -27)
+  # Sample size column
+  text(-22, 12, font = 2, pos = 2, cex = 1, "n")
+  n_text(LS_vBMD, -22)
+  # Weight column
+  text(34, 12, font = 2, pos = 2, cex = 1, "Weight")
+  weights_text(LS_vBMD_model, 34)
+  text(34, -1, font = 1, pos = 2, cex = 1, "100.0%")
+  # Heterogeneity text
+  text(
+    -69.9, -2.1, pos = 4, cex = 0.85,
+    bquote(
+      paste(
+        "Heterogeneity: ",
+        I^2, " = ",
+        .(
+          formatC(i2(LS_vBMD, LS_vBMD_model), digits = 0, format = "f")
+          ), "%, ",
+        italic(.("p")), " < 0.001"
+      )
+    )
+  )
+  # Overall effect text
+  text(
+    -69.9, -3.0, pos = 4, cex = 0.85,
+    bquote(
+      paste(
+        "Test for overall effect: ",
+        "z = ",
+        .(formatC(LS_vBMD_model$zval, digits = 2, format = "f")), ", ",
+        italic(.("p")), " < 0.001 "
+      )
+    )
+  )
+}
+
 # Radius vBMD forest plot -------------------------------------------------
 
 make_radius_plot <- function() {
@@ -138,132 +258,12 @@ make_tibia_plot <- function() {
   )
 }
 
-# Lumbar spine vBMD forest plot -------------------------------------------
-
-make_LS_plot <- function() {
-  par(mar = c(2.5, 2, 0.5, 2))
-  forest(
-    LS_vBMD_model,
-    slab = LS_vBMD$study,
-    header = TRUE,
-    digits = 1,
-    xlab = "",
-    mlab = "Random effects model",
-    xlim = c(-70, 59),
-    top = 4
-  )
-  # X axis label
-  text(15, -5.5, font = 1, pos = 2, cex = 1, "Mean percentage change")
-  # Plot title
-  text(
-    -70.5, 13.2, font = 2, pos = 4, cex = 1,
-    "Lumbar spine volumetric bone mineral density"
-  )
-  # Follow-up time column
-  text(-26, 12, font = 2, pos = 2, cex = 1, "Follow-up (mo.)")
-  followup_text(LS_vBMD, -27)
-  # Sample size column
-  text(-22, 12, font = 2, pos = 2, cex = 1, "n")
-  n_text(LS_vBMD, -22)
-  # Weight column
-  text(34, 12, font = 2, pos = 2, cex = 1, "Weight")
-  weights_text(LS_vBMD_model, 34)
-  text(34, -1, font = 1, pos = 2, cex = 1, "100.0%")
-  # Heterogeneity text
-  text(
-    -69.9, -2.1, pos = 4, cex = 0.85,
-    bquote(
-      paste(
-        "Heterogeneity: ",
-        I^2, " = ",
-        .(
-          formatC(i2(LS_vBMD, LS_vBMD_model), digits = 0, format = "f")
-          ), "%, ",
-        italic(.("p")), " < 0.001"
-      )
-    )
-  )
-  # Overall effect text
-  text(
-    -69.9, -3.0, pos = 4, cex = 0.85,
-    bquote(
-      paste(
-        "Test for overall effect: ",
-        "z = ",
-        .(formatC(LS_vBMD_model$zval, digits = 2, format = "f")), ", ",
-        italic(.("p")), " < 0.001 "
-      )
-    )
-  )
-}
-
-# Total hip vBMD forest plot -------------------------------------------
-
-make_TH_plot <- function() {
-  par(mar = c(3.5, 2, 1, 2))
-  forest(
-    TH_vBMD_model,
-    slab = TH_vBMD$study,
-    header = TRUE,
-    digits = 1,
-    xlab = "",
-    mlab = "Random effects model",
-    xlim = c(-70, 59),
-    top = 4
-  )
-  # X axis label
-  text(11, -5, font = 1, pos = 2, cex = 1, "Mean percentage change")
-  # Plot title
-  text(
-    -70.5, 6, font = 2, pos = 4, cex = 1,
-    "Total hip volumetric bone mineral density"
-  )
-  # Follow-up time column
-  text(-26, 5, font = 2, pos = 2, cex = 1, "Follow-up (mo.)")
-  followup_text(TH_vBMD, -27)
-  # Sample size column
-  text(-22, 5, font = 2, pos = 2, cex = 1, "n")
-  n_text(TH_vBMD, -22)
-  # Weight column
-  text(34, 5, font = 2, pos = 2, cex = 1, "Weight")
-  weights_text(TH_vBMD_model, 34)
-  text(34, -1, font = 1, pos = 2, cex = 1, "100.0%")
-  # Heterogeneity text
-  text(
-    -69.9, -2.1, pos = 4, cex = 0.85,
-    bquote(
-      paste(
-        "Heterogeneity: ",
-        I^2, " = ",
-        .(
-          formatC(i2(TH_vBMD, TH_vBMD_model), digits = 0, format = "f")
-          ), "%, ",
-        italic(.("p")), " = ",
-        .(formatC(TH_vBMD_model$QEp, digits = 3, format = "f"))
-      )
-    )
-  )
-  # Overall effect text
-  text(
-    -69.9, -3.0, pos = 4, cex = 0.85,
-    bquote(
-      paste(
-        "Test for overall effect: ",
-        "z = ",
-        .(formatC(TH_vBMD_model$zval, digits = 2, format = "f")), ", ",
-        italic(.("p")), " = ",
-        .(formatC(TH_vBMD_model$pval, digits = 3, format = "f"))
-      )
-    )
-  )
-}
-
 # Combine and save plots --------------------------------------------------
 
-p1 <- as.ggplot(~ make_radius_plot())
-p2 <- as.ggplot(~ make_tibia_plot())
-p3 <- as.ggplot(~ make_LS_plot())
-p4 <- as.ggplot(~ make_TH_plot())
+p1 <- as.ggplot(~ make_TH_plot())
+p2 <- as.ggplot(~ make_LS_plot())
+p3 <- as.ggplot(~ make_radius_plot())
+p4 <- as.ggplot(~ make_tibia_plot())
 agg_png(
   here("figures", "forest_plots.png"),
   width = 25,
@@ -271,5 +271,5 @@ agg_png(
   units = "cm",
   res = 300
 )
-plot_grid(p1, p2, p3, p4, ncol = 1, rel_heights = c(1, 1, 0.95, 0.6))
+plot_grid(p1, p2, p3, p4, ncol = 1, rel_heights = c(0.6, 0.95, 1, 1))
 dev.off()
